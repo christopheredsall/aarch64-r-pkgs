f97e36871f4bb2068f702b0d1c9e589a0411514c
Ainsley Rutterford <ar16478@bristol.ac.uk>
2019-08-21 10:21:04 +0000

    Added pipeline.yml

--- add_pkgs.sh
+++ add_pkgs.sh
@@ -1,6 +1,8 @@
-for pkg in foo bar
+file="packages.txt"
+packages=`cat $file`
+for pkg in $packages
 do
-cat > ${pkg}.r <<EOT
+cat > package_scripts/${pkg}.r <<EOT
 install.packages('${pkg}', repos="https://www.stats.bris.ac.uk/R/")
 library('${pkg}')
 EOT
