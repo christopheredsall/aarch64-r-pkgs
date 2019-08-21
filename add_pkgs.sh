30e016e8f02630bf5539fb93365b46267a0a8ef6
Ainsley Rutterford <ar16478@bristol.ac.uk>
2019-08-21 11:30:13 +0000

    Edited add_pkgs.sh file

--- add_pkgs.sh
+++ add_pkgs.sh
@@ -1,9 +1,9 @@
 file="packages.txt"
 packages=`cat $file`
-for pkg in $packages
-do
-cat > package_scripts/${pkg}.r <<EOT
-install.packages('${pkg}', repos="https://www.stats.bris.ac.uk/R/")
-library('${pkg}')
-EOT
+
+for pkg in $packages do
+    cat > ${pkg}.r <<EOT
+    install.packages('${pkg}', repos="https://www.stats.bris.ac.uk/R/")
+    library('${pkg}')
+    EOT
 done
