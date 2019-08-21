67d772a510942eaadf350cc65a27b74842980e32
Ainsley Rutterford <ar16478@bristol.ac.uk>
2019-08-21 11:19:39 +0000

    Trying simple pipeline.sh

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -10,13 +10,16 @@ packages = `cat $file`
 echo "steps:"
 
 # add a new test for every package in packages.txt
-for pkg in $packages do
-    cat > ${pkg}.r << EOT
-    install.packages('${pkg}', repos="https://www.stats.bris.ac.uk/R/")
-    library('${pkg}')
-    EOT
-    echo "  - label: ${pkg}"
-    echo "    command: ./driver.sh ${pkg}.r"
-    echo "    soft_fail:"
-    echo "      - exit_status: 1"
-done
+# for pkg in $packages do
+#     cat > ${pkg}.r << EOT
+#     install.packages('${pkg}', repos="https://www.stats.bris.ac.uk/R/")
+#     library('${pkg}')
+#     EOT
+#     echo "  - label: ${pkg}"
+#     echo "    command: ./driver.sh ${pkg}.r"
+#     echo "    soft_fail:"
+#     echo "      - exit_status: 1"
+# done
+
+echo "  - label: \"test\""
+echo "    command: echo \"TEST\""
