4957344633a7010d8f4d0fc521e4b631a59684bf
Ainsley Rutterford <ar16478@bristol.ac.uk>
2019-08-21 11:10:35 +0000

    Added pipeline.sh which will generate a pipeline.yml dynamically

---
+++ .buildkite/pipeline.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# exit immediately on failure, or if an undefined variable is used
+set -eu
+
+file     = "packages.txt"
+packages = `cat $file`
+
+# begin the pipeline.yml file
+echo "steps:"
+
+# add a new test for every package in packages.txt
+for pkg in $packages do
+    cat > ${pkg}.r << EOT
+    install.packages('${pkg}', repos="https://www.stats.bris.ac.uk/R/")
+    library('${pkg}')
+    EOT
+    echo "  - label: ${pkg}"
+    echo "    command: ./driver.sh ${pkg}.r"
+    echo "    soft_fail:"
+    echo "      - exit_status: 1"
+done
