e0c6b6a8f91d85961be65338091c1e0ef9fd8e4c
Christopher Edsall <chris.edsall@bristol.ac.uk>
2019-08-26 19:22:32 +0100

    Hardcode environment for Isambard

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -8,16 +8,24 @@ mapfile -t packages < packages.txt
 # begin the pipeline.yml file
 echo "steps:"
 
+# Isambard
+#MODULESHOME=/usr/share/Modules/3.2.10
+#RMODULE=cray-R/3.4.2
+
+# Catalyst
+export MODULESHOME=/opt/cray/pe/modules/3.2.11.2
+export RMODULE=lang/r/3.6.0-gcc
+
+
 for pkg in ${packages[@]}
 do
 echo "  - label: ${pkg}"
 echo "    commands:"
-echo "        - MODULESHOME=/usr/share/Modules/3.2.10"
 echo "        - source ${MODULESHOME}/init/bash"
-echo "        - module load lang/r/3.6.0-gcc"
+echo "        - module load ${RMODULE}"
 echo "        - echo \"if (!require('${pkg}')) { install.packages('${pkg}', repos='https://www.stats.bris.ac.uk/R/') }; library('${pkg}')\" | R --vanilla --quiet"
 echo "    agents:"
-echo "      queue: 'catalyst'"
+echo "      queue: 'isambard'"
 echo "    soft_fail:"
 echo "        - exit_status: 1"
 done
