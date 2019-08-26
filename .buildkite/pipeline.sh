16244efa42fa6e7ce1ba7f83e46b04f5f65982fc
Christopher Edsall <chris.edsall@bristol.ac.uk>
2019-08-26 21:14:00 +0100

    Move both queues in to a loop in pipeline.sh

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -8,23 +8,28 @@ mapfile -t packages < packages.txt
 # begin the pipeline.yml file
 echo "steps:"
 
-# Isambard
-#MODULESHOME=/usr/share/Modules/3.2.10
-#RMODULE=lang/r/3.6.0-gcc
-
-# Catalyst
-export MODULESHOME=/opt/cray/pe/modules/3.2.11.2
-export RMODULE=cray-R/3.4.2
-
-for pkg in ${packages[@]}
+for queue in isambard catalyst
 do
-echo "  - label: ${pkg}"
-echo "    commands:"
-echo "        - source ${MODULESHOME}/init/bash"
-echo "        - module load ${RMODULE}"
-echo "        - echo \"if (!require('${pkg}')) { install.packages('${pkg}', repos='https://www.stats.bris.ac.uk/R/') }; library('${pkg}')\" | R --vanilla --quiet"
-echo "    agents:"
-echo "      queue: 'isambard'"
-echo "    soft_fail:"
-echo "        - exit_status: 1"
+    case $queue in
+    isambard)
+        export MODULESHOME=/opt/cray/pe/modules/3.2.11.2
+        export RMODULE=cray-R/3.4.2
+        ;;
+    catalyst)
+        export MODULESHOME=/usr/share/Modules/3.2.10
+        export RMODULE=lang/r/3.6.0-gcc
+        ;;
+    esac
+    for pkg in "${packages[@]}"
+    do
+        echo "  - label: ${pkg}"
+        echo "    commands:"
+        echo "        - source ${MODULESHOME}/init/bash"
+        echo "        - module load ${RMODULE}"
+        echo "        - echo \"if (!require('${pkg}')) { install.packages('${pkg}', repos='https://www.stats.bris.ac.uk/R/') }; library('${pkg}')\" | nice R --vanilla --quiet"
+        echo "    agents:"
+        echo "      queue: ${queue}"
+        echo "    soft_fail:"
+        echo "        - exit_status: 1"
+    done
 done
