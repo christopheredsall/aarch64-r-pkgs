91eff019b55dd5fba878ae993d7edcc00352a34a
Christopher Edsall <chris.edsall@bristol.ac.uk>
2019-09-02 15:02:57 +0000

    Support multiple package repositories
    There is more to the R package landscape than just CRAN.
    For instance there are repositories Bioconductor and RForge.
    Also, via the devtools package R supports loading packages from
    source control repositories like GitHub and even from bare files.
    
    This change converts from simple package list to a .csv file and
    alters the pipeline shell script to parse it.
    
    A later change will implement the logic for the different repositories

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -3,7 +3,23 @@
 # exit immediately on failure, or if an undefined variable is used
 set -eu
 
-mapfile -t packages < packages.txt
+packages=()
+declare -A repositories
+declare -A organisations
+
+# read a commma separated table of packages, three columns
+# package_name, repository, organiaisation
+# (github packages require an organisaition to install)
+
+OLD_IFS=$IFS
+IFS=","
+while read -r name repo org 
+do
+    packages+=("${name}")
+    repositories[${name}]=${repo}
+    organisations[${name}]=${org}
+done < packages.csv
+IFS=${OLD_IFS}
 
 # begin the pipeline.yml file
 echo "steps:"
@@ -22,14 +38,17 @@ do
     esac
     for pkg in "${packages[@]}"
     do
-        echo "  - label: ${pkg}"
-        echo "    commands:"
-        echo "        - source ${MODULESHOME}/init/bash"
-        echo "        - module load ${RMODULE}"
-        echo "        - echo \"if (!require('${pkg}')) { install.packages('${pkg}', repos='https://www.stats.bris.ac.uk/R/') }; library('${pkg}')\" | nice R --vanilla --quiet"
-        echo "    agents:"
-        echo "      queue: ${queue}"
-        echo "    soft_fail:"
-        echo "        - exit_status: 1"
+        # for the meantime only do CRAN
+        if [ "${repositories[${pkg}]}" == "CRAN" ]; then
+            echo "  - label: ${pkg}"
+            echo "    commands:"
+            echo "        - source ${MODULESHOME}/init/bash"
+            echo "        - module load ${RMODULE}"
+            echo "        - echo \"if (!require('${pkg}')) { install.packages('${pkg}', repos='https://www.stats.bris.ac.uk/R/') }; library('${pkg}')\" | nice R --vanilla --quiet"
+            echo "    agents:"
+            echo "      queue: ${queue}"
+            echo "    soft_fail:"
+            echo "        - exit_status: 1"
+        fi
     done
 done
